698984620103c1e4209ae79fd875ac3a41972b2f
EgorStrupinskiy <venik2405@yandex.ru>
2023-03-12 16:39:09 +0300

    Intervals.java and Main.java added

---
+++ src/main/java/com/innowise/intervals/Intervals.java
@@ -0,0 +1,15 @@
+package com.innowise.intervals;
+
+
+public class Intervals {
+
+    public static String intervalConstruction(String[] arr) {
+        return null;
+    }
+
+
+    public static String intervalIdentification(String[] arr) {
+        return null;
+    }
+
+}
