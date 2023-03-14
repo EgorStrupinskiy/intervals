2fef33b7baa00fe4dcfead880273d6fdea2b5b0a
EgorStrupinskiy <venik2405@yandex.ru>
2023-03-14 13:21:49 +0300

    Small code readability improvements implemented, javadoc added

--- src/main/java/com/innowise/intervals/Intervals.java
+++ src/main/java/com/innowise/intervals/Intervals.java
@@ -1,14 +1,40 @@
-package com.innowise.intervals;
+/**
 
+ The Intervals class provides methods for interval construction and interval identification.
+ */
+package com.innowise.intervals;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class Intervals {
-    private static final Map<String, Integer> SEMITONES;
+    /**
+     * The map SEMITONES contains a set of notes with their semitone numbers.
+     */
+     private static final Map<String, Integer> SEMITONES;
+
+     /**
+     * The array NOTES contains all the notes that can be used.
+     */
     private static final String[] NOTES = {"C", "D", "E", "F", "G", "A", "B"};
+
+    /**
+     * NOTES_COUNT represents the count of all the notes that can be used.
+     */
+    private static final int NOTES_COUNT = 7;
+
+    /**
+     * SEMITONES_COUNT represents the count of all semitones.
+     */
+    private static final int SEMITONES_COUNT = 12;
+
+    /**
+     * The map INTERVALS contains a set of intervals with their corresponding semitone distances.
+     */
     private static final Map<String, Integer> INTERVALS;
 
     static {
@@ -63,27 +89,34 @@ public class Intervals {
         INTERVALS.put("P8", 12);
     }
 
+    /**
+     Calculates the end note of an interval construction based on the interval and start note provided.
+     @param arr An array of strings containing the interval and start note in the format [interval, startNote].
+     @return The end note of the interval construction.
+     @throws IllegalArgumentException If the input is invalid.
+     */
     public static String intervalConstruction(String[] arr) {
         checkArgumentsCount(arr);
 
         final var interval = arr[0];
         final var startNote = arr[1];
 
-        final var startNoteIndex = SEMITONES.get(startNote);
-        final var semitones = INTERVALS.get(interval);
+        final var semitonesToMove = INTERVALS.get(interval);
+        final var startNoteSemitoneIndex = SEMITONES.get(startNote);
 
-        final var startNodeDegree = getNotePosition(startNote.charAt(0));
+        final var startNodeDegree = getNotePositionInSequence(startNote.charAt(0));
         final var degreesToMove = Integer.parseInt(String.valueOf(interval.charAt(1)));
-        var endNoteDegreeIndex = (checkIfOrderIsAscending(arr) ? startNodeDegree + degreesToMove - 2 : startNodeDegree - degreesToMove) % NOTES.length;
+        var endNoteDegreeIndex = (checkIfOrderIsAscending(arr) ? startNodeDegree + degreesToMove - 2 : startNodeDegree - degreesToMove) % NOTES_COUNT;
 
-        if (endNoteDegreeIndex < 0){
-            endNoteDegreeIndex += NOTES.length;
+        if (endNoteDegreeIndex < 0) {
+            endNoteDegreeIndex += NOTES_COUNT;
         }
-        final var endNoteSemitoneIndex = new AtomicInteger((checkIfOrderIsAscending(arr) ? startNoteIndex + semitones : startNoteIndex - semitones) % 12);
+
+        final var endNoteSemitoneIndex = new AtomicInteger((checkIfOrderIsAscending(arr) ? startNoteSemitoneIndex + semitonesToMove : startNoteSemitoneIndex - semitonesToMove) % SEMITONES_COUNT);
         final var endNoteDegree = NOTES[endNoteDegreeIndex + 1];
 
         if (endNoteSemitoneIndex.get() < 0) {
-            endNoteSemitoneIndex.set(endNoteSemitoneIndex.get() + 12);
+            endNoteSemitoneIndex.set(endNoteSemitoneIndex.get() + SEMITONES_COUNT);
         }
 
         return SEMITONES.entrySet().stream()
@@ -94,24 +127,31 @@ public class Intervals {
                 .orElseThrow(() -> new IllegalArgumentException("Invalid input"));
     }
 
+    /**
+
+     Identifies the interval between two notes
+     @param arr An array of strings with the start note and end note in the format [startNote, endNote].
+     @return The interval between the start note and end note.
+     @throws IllegalArgumentException If the input is invalid.
+     */
     public static String intervalIdentification(String[] arr) {
         checkArgumentsCount(arr);
 
-        final var startNoteIndex = SEMITONES.get(arr[0]);
-        var endNoteIndex = SEMITONES.get(arr[1]);
-        if (startNoteIndex > endNoteIndex) {
-            endNoteIndex += 12;
+        final var startNoteSemitoneIndex = SEMITONES.get(arr[0]);
+        var endNoteSemitoneIndex = SEMITONES.get(arr[1]);
+
+        if (startNoteSemitoneIndex > endNoteSemitoneIndex) {
+            endNoteSemitoneIndex += SEMITONES_COUNT;
         }
 
-        final var semitones = new AtomicInteger(Math.abs(endNoteIndex - startNoteIndex) % SEMITONES.size());
+        final var semitonesInInterval = new AtomicInteger(Math.abs(endNoteSemitoneIndex - startNoteSemitoneIndex) % SEMITONES.size());
 
         if (!checkIfOrderIsAscending(arr)) {
-            semitones.set(-semitones.get());
-            semitones.set(semitones.get() + 12);
+            semitonesInInterval.set(-semitonesInInterval.get() + SEMITONES_COUNT);
         }
 
         return INTERVALS.entrySet().stream()
-                .filter(entry -> entry.getValue().equals(semitones.get()))
+                .filter(entry -> entry.getValue().equals(semitonesInInterval.get()))
                 .map(Map.Entry::getKey)
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Invalid input"));
@@ -134,10 +174,10 @@ public class Intervals {
         return true;
     }
 
-    private static int getNotePosition(char note) {
+    private static int getNotePositionInSequence(char note) {
         var position = -1;
-        for (var i = 0; i < NOTES.length; i++){
-            if (Objects.equals(String.valueOf(note), NOTES[i])){
+        for (var i = 0; i < NOTES_COUNT; i++) {
+            if (Objects.equals(String.valueOf(note), NOTES[i])) {
                 position = i;
             }
         }
